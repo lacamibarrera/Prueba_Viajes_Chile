537773d7caf413e5a20db8ce902a61073b2274fa
lacamibarrera <camibarrera@gmail.com>
2024-07-11 12:56:15 -0400

    se agrego Carrusel de imagenes y funcionalidad de movimiento con js, se importo jquery

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#carouselExampleFade').carousel({
+      interval: 3000, // Cambia este valor para ajustar la velocidad de las transiciones (en milisegundos)
+      ride: 'carousel'
+    });
+  });
